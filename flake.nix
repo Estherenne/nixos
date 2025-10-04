e6c6ba1a76c2947afc635914d58331ae3f138703
Estherenne <cyclone278@proton.me>
2025-10-04 08:58:09 +0200

    update hostname

--- flake.nix
+++ flake.nix
@@ -4,9 +4,11 @@
   nixConfig = {
     extra-substituters = [
       "https://vicinae.cachix.org"
+      "https://nix-community.cachix.org"
     ];
     extra-trusted-public-keys = [
       "vicinae.cachix.org-1:1kDrfienkGHPYbkpNj1mWTr7Fm1+zcenzgTizIcI3oc="
+      "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
     ];
   };
 
@@ -27,7 +29,7 @@
   };
 
   outputs = { self, nixpkgs, home-manager, vicinae, ... }@inputs: { 
-    nixosConfigurations.nixos = nixpkgs.lib.nixosSystem {
+    nixosConfigurations.comet = nixpkgs.lib.nixosSystem {
       specialArgs = { 
         inherit inputs;
       };
